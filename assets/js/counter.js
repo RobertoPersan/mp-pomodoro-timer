062c02898d91f98c868bcadd46f7cf828ce8fabe
José Roberto Pereira Dos Santos <rob_berty@hotmail.com>
2025-04-24 14:23:06 -0300

    Projeto pomodoro em js

--- assets/js/counter.js
+++ assets/js/counter.js
@@ -1,1 +1,22 @@
-// Faça a lógica referente o cronômetro!
+const pomodoreSelect = document.querySelector("#pomodoro");
+const shortBreakSelect = document.querySelector("#shot-break");
+const LongBreakSelect = document.querySelector("#long-break");
+const startButton = document.querySelector("#start");
+const timerParagraph = document.querySelector("#counter");
+
+let selectedTimer = "pomodoro"; //pomodoro, shot-break, long-break
+
+function changeSelectClasses(timer){
+    if (timer === "pomodoro") {
+        pomodoroSelect.classList.add("active");
+        shortBreakSelect.classList.remove("actve");
+        localStorage.classList.remove("active");
+    }
+}
+
+function selectedTimer(timer) {
+    selectedTimer = timer;
+
+    changeSelectClasses(timer);
+    changeTimerValue(timer);
+}
