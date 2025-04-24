b279e4693f543bac14889ae64f48d00202e3e4df
José Roberto Pereira Dos Santos <rob_berty@hotmail.com>
2025-04-24 14:41:52 -0300

    Criando a função converter segundos

--- assets/js/counter.js
+++ assets/js/counter.js
@@ -8,10 +8,38 @@ let selectedTimer = "pomodoro"; //pomodoro, shot-break, long-break
 
 function changeSelectClasses(timer){
     if (timer === "pomodoro") {
-        pomodoroSelect.classList.add("active");
-        shortBreakSelect.classList.remove("actve");
-        localStorage.classList.remove("active");
+        pomodoroSelect.classList.add("active-button");
+        shortBreakSelect.classList.remove("actve-button");
+        localStorage.classList.remove("active-button");
+    } else if (timer === "shot-break") {
+        pomodoreSelect.classList.remove("active-button");
+        shortBreakSelect.classList.add("active-button");
+        LongBreakSelect.classList.remove("active-button");
+    } else if (timer === "long-break") {
+        pomodoreSelect.classList.remove("active-button");
+        shortBreakSelect.classList.remove("active-button");
+        LongBreakSelect.classList.add("active-button");
     }
+}
+// Função que converte os segundos em minutos
+function secondsToMinutesSeconds(totalSeconds) {
+    const minutes = Math.floor(seconds / 60);
+    const seconds = seconds % 60;
+
+    const padSeconds = seconds.toString().padStart(2, "0");
+    return `${minutes}: ${padSeconds}`;
+}
+
+function getTimerValuer(timer) {
+    return {
+        pomodoro: 25 * 60,
+        "short-break": 5 * 60,
+        "long-break": 25 * 60,
+    }[timer]
+}
+
+function changeTimerValue(timer) {
+    timerParagraph.textContent = secondsToMinutesSeconds(getTimerValuer(timer));
 }
 
 function selectedTimer(timer) {
